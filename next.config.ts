0c74e07b9a359c2ca4fea44162738e3c7384a865
Yilia006 <leejoying@163.com>
2026-07-11 22:36:30 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // 允许本地网络访问
-  allowedDevOrigins: ['localhost', '127.0.0.1'],
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
